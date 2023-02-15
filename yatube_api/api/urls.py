e3db8ab7cc5a027cc16f02c97a9531f7e39c7f28
Денис Набатов <nabatoffdenis@yandex.ru>
2023-02-15 14:46:11 +0300

    api

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from api.views import PostViewSet, GroupViewSet, CommentViewSet
+
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+router.register(r'api/v1/posts', PostViewSet, basename='posts')
+router.register(r'api/v1/groups', GroupViewSet, basename='groups')
+router.register(r'api/v1/posts/(?P<post_id>\d+)/comments',
+                CommentViewSet,
+                basename='comments')
+
+urlpatterns = [
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('', include(router.urls))
+]
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,23 +2,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from rest_framework.authtoken import views
+# from rest_framework import routers
+# from rest_framework.authtoken import views
 
-from api.views import PostViewSet, GroupViewSet, CommentViewSet
+# from api.views import PostViewSet, GroupViewSet, CommentViewSet
 
 
-router = routers.DefaultRouter()
-router.register(r'api/v1/posts', PostViewSet, basename='posts')
-router.register(r'api/v1/groups', GroupViewSet, basename='groups')
-router.register(r'api/v1/posts/(?P<post_id>\d+)/comments',
-                CommentViewSet,
-                basename='comments')
+# router = routers.DefaultRouter()
+# router.register(r'api/v1/posts', PostViewSet, basename='posts')
+# router.register(r'api/v1/groups', GroupViewSet, basename='groups')
+# router.register(r'api/v1/posts/(?P<post_id>\d+)/comments',
+#                 CommentViewSet,
+#                 basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls))
+    path('', include('api.urls', namespace='api'))
 ]
 
 
